e1643e7a14bb2801f3964982ff84b434ffab39dc
missing-q <ivybetterit@gmail.com>
2023-08-01 15:48:47 -0500

    Function cleanup

--- module/disable-hooks.js
+++ module/disable-hooks.js
@@ -47,56 +47,14 @@ export class DisableHooks {
             if (item.system.active != (undefined)){
                 //evaluate HP modification at interval
                 if ((item.system.modHP.value != "") && (item.system.active.state) && (active.findIndex(i => i == item.system.modHP.timing) != -1)){
-                    console.log("hello!! we're in here!!")
-                    let num = 0;
-                    try {
-                        num = item.system.modHP.value
-                        if (num.indexOf('@level') != -1){
-                            num = num.replace("@level", item.system.level.init);
-                        }
-                        if (num.indexOf('D') != -1){
-                            
-                            let front = num.substring(0,num.indexOf('D'))
-                            let back = num.substring(num.indexOf('D')+1)
-                            front += "d10"
-                            let roll = new Roll(front);
-                            await roll.evaluate({async: true});
-                            num = roll.total + back
-                        }
-                        num = math.evaluate(num)
-                        let chatData = {"content": actor.name + " (" + num + ")", "speaker": ChatMessage.getSpeaker({ actor: actor })};
-                        ChatMessage.create(chatData);
-                    } catch (error) {
-                        console.error("Values other than formula, @level, D dice are not allowed.");
-                    }
-                    console.log(num)
+                    console.log("HP applied")
+                    let num = evalEffect(effect, "HP")
                     actorupdates["system.attributes.hp.value"] = actor.system.attributes.hp.value + num
                 }
                 //evaluate encroach modification at interval
                 if ((item.system.modEncroach.value != "") && (item.system.active.state) && (active.findIndex(i => i == item.system.modEncroach.timing) != -1)){
-                    console.log("hello!! we're in here!!")
-                    let num = 0;
-                    try {
-                        num = item.system.modEncroach.value
-                        if (num.indexOf('@level') != -1){
-                            num = num.replace("@level", item.system.level.init);
-                        }
-                        if (num.indexOf('D') != -1){
-                            
-                            let front = num.substring(0,num.indexOf('D'))
-                            let back = num.substring(num.indexOf('D')+1)
-                            front += "d10"
-                            let roll = new Roll(front);
-                            await roll.evaluate({async: true});
-                            num = roll.total + back
-                        }
-                        num = math.evaluate(num)
-                        let chatData = {"content": `<div class="context-box">${actor.name}: ${actor.system.attributes.encroachment.value} -> ${actor.system.attributes.encroachment.value + num} (+${num})</div>`, "speaker": ChatMessage.getSpeaker({ actor: actor })};
-                        ChatMessage.create(chatData);
-                    } catch (error) {
-                        console.error("Values other than formula, @level, D dice are not allowed.");
-                    }
-                    console.log(num)
+                    console.log("encroach self")
+                    let num = evalEffect(effect, "encroach")
                     actorupdates["system.attributes.encroachment.value"] = actor.system.attributes.encroachment.value + num
                 }
                 if (active.findIndex(i => i == item.system.active.disable) != -1){
@@ -110,60 +68,14 @@ export class DisableHooks {
             console.log(effect)
             //apply HP effect
             if (active.findIndex(i => i == effect.modHP.timing) != -1){
-                console.log("hello!! we're in here!!")
-                let num = 0;
-                try {
-                    num = effect.modHP.value
-                    if (num.indexOf('@level') != -1){
-                        let tmp = game.actors.get(effect.actorId).items.get(effect.itemId)
-                        num = num.replace("@level", tmp.system.level.init);
-                    }
-                    if (num.indexOf('D') != -1){
-                        
-                        let front = num.substring(0,num.indexOf('D'))
-                        let back = num.substring(num.indexOf('D')+1)
-                        front += "d10"
-                        let roll = new Roll(front);
-                        await roll.evaluate({async: true});
-                        num = roll.total + back
-                    }
-                    num = math.evaluate(num)
-                    let chatData = {"content": actor.name + " (" + num + ")", "speaker": ChatMessage.getSpeaker({ actor: actor })};
-                    ChatMessage.create(chatData);
-                } catch (error) {
-                    console.log(error)
-                    console.error("Values other than formula, @level, D dice are not allowed.");
-                }
-                console.log(num)
+                console.log("HP applied")
+                let num = evalEffect(effect, "HP")
                 actorupdates["system.attributes.hp.value"] = actor.system.attributes.hp.value + num
             }
             //apply encroach effect
             if (active.findIndex(i => i == effect.modEncroach.timing) != -1){
-                console.log("hello!! we're in here!!")
-                let num = 0;
-                try {
-                    num = effect.modEncroach.value
-                    if (num.indexOf('@level') != -1){
-                        let tmp = game.actors.get(effect.actorId).items.get(effect.itemId)
-                        console.log(tmp)
-                        num = num.replace("@level", tmp.system.level.init);
-                    }
-                    if (num.indexOf('D') != -1){
-                        
-                        let front = num.substring(0,num.indexOf('D'))
-                        let back = num.substring(num.indexOf('D')+1)
-                        front += "d10"
-                        let roll = new Roll(front);
-                        await roll.evaluate({async: true});
-                        num = roll.total + back
-                    }
-                    num = math.evaluate(num)
-                    let chatData = {"content": `<div class="context-box">${actor.name}: ${actor.system.attributes.encroachment.value} -> ${actor.system.attributes.encroachment.value + num} (+${num})</div>`, "speaker": ChatMessage.getSpeaker({ actor: actor })};
-                    ChatMessage.create(chatData);
-                } catch (error) {
-                    console.error("Values other than formula, @level, D dice are not allowed.");
-                }
-                console.log(num)
+                console.log("encroach applied")
+                let num = evalEffect(effect, "encroach")
                 actorupdates["system.attributes.encroachment.value"] = actor.system.attributes.encroachment.value + num
             }
             //negate applied
@@ -173,5 +85,35 @@ export class DisableHooks {
         }
         actor.update(actorupdates);
     }
-
+    static async evalEffect(effect, type){
+        let num = 0
+        try {
+            if (type == "HP"){
+                num = effect.modHP.value
+            } else {
+                num = effect.modEncroach.value
+            }
+            if (num.indexOf('@level') != -1){
+                let tmp = game.actors.get(effect.actorId).items.get(effect.itemId)
+                console.log(tmp)
+                num = num.replace("@level", tmp.system.level.init);
+            }
+            if (num.indexOf('D') != -1){
+                
+                let front = num.substring(0,num.indexOf('D'))
+                let back = num.substring(num.indexOf('D')+1)
+                front += "d10"
+                let roll = new Roll(front);
+                await roll.evaluate({async: true});
+                num = roll.total + back
+            }
+            num = math.evaluate(num)
+            let chatData = {"content": `<div class="context-box">${actor.name}: ${actor.system.attributes.encroachment.value} -> ${actor.system.attributes.encroachment.value + num} (+${num})</div>`, "speaker": ChatMessage.getSpeaker({ actor: actor })};
+            ChatMessage.create(chatData);
+        } catch (error) {
+            console.error("Values other than formula, @level, D dice are not allowed.");
+            num = 0
+        }
+        return num;
+    }
 }
